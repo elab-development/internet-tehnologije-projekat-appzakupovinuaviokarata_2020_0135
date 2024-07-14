48a0fd83c6b4e2358a998f0c22d3cf639f00f099
Vladimir Petrovic <petrovicv2606@gmail.com>
2024-07-14 16:58:35 +0200

    scss za register i login

--- flight-booking-app/src/app/app.module.ts
+++ flight-booking-app/src/app/app.module.ts
@@ -7,22 +7,25 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MaterialModule } from './material/material.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { Token } from '@angular/compiler';
 import { TokenInterceptor } from './token.interceptor';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
     ReactiveFormsModule,
   ],
   providers: [
@@ -32,8 +35,8 @@ import { RegisterComponent } from './register/register.component';
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true,
-    }
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
