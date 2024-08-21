bc6a55ff33c12771d091699e2cd87a34e65252a7
Vladimir Petrovic <petrovicv2606@gmail.com>
2024-08-22 00:55:24 +0200

    dogradjeni sidebar i navbar, dodat logout u sidebar

--- flight-booking-app/src/app/components/sidebar/sidebar.component.ts
+++ flight-booking-app/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem('auth_token');
+        localStorage.removeItem('user');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Logout failed', err);
+      },
+    });
+  }
 }
