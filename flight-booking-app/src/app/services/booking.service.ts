55adec1e5110ae8225f01cd0e939f96a1bc0a938
Vladimir Petrovic <petrovicv2606@gmail.com>
2024-08-23 22:25:07 +0200

    resavanje konflikta v1

--- flight-booking-app/src/app/services/booking.service.ts
+++ flight-booking-app/src/app/services/booking.service.ts
@@ -1,4 +1,3 @@
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
@@ -8,27 +7,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class BookingService {
-// <<<<<<< jovana
-//   url: string = 'http://127.0.0.1:8000/api';
+  // <<<<<<< jovana
+  //   url: string = 'http://127.0.0.1:8000/api';
 
-//   constructor(private http: HttpClient) { }
+  //   constructor(private http: HttpClient) { }
 
-//   private getHeaders(): HttpHeaders {
-//     const token = localStorage.getItem('token'); // Adjust as needed to get the token
-//     let headers = new HttpHeaders();
-//     headers = headers.set('Authorization', `Bearer ${token}`);
-//     return headers;
-//   }
-//   confirmBooking(userId: number, flightId: number): Observable<any> {
-//     const bookingData = {
-//       flight_id: flightId,
-//       status: 'confirmed', // Set the status as needed
-//     };
-//     return this.http.post(`${this.url}/bookings`, bookingData, { headers: this.getHeaders() });
-//   }
-//   getBookings(): Observable<any> {
-//     return this.http.get(`${this.url}/bookings`);
-// =======
+  //   private getHeaders(): HttpHeaders {
+  //     const token = localStorage.getItem('token'); // Adjust as needed to get the token
+  //     let headers = new HttpHeaders();
+  //     headers = headers.set('Authorization', `Bearer ${token}`);
+  //     return headers;
+  //   }
+  //   confirmBooking(userId: number, flightId: number): Observable<any> {
+  //     const bookingData = {
+  //       flight_id: flightId,
+  //       status: 'confirmed', // Set the status as needed
+  //     };
+  //     return this.http.post(`${this.url}/bookings`, bookingData, { headers: this.getHeaders() });
+  //   }
+  //   getBookings(): Observable<any> {
+  //     return this.http.get(`${this.url}/bookings`);
+  // =======
   private baseUrl = 'http://localhost:8000/api/bookings';
   private baseUrlData = 'http://localhost:8000/api/bookingsall';
   constructor(private http: HttpClient) {}
@@ -55,6 +54,13 @@ export class BookingService {
 
   deleteBooking(id: number): Observable<any> {
     return this.http.delete<any>(`${this.baseUrl}/${id}`);
-// >>>>>>> main
+  }
+
+  confirmBooking(userId: number, flightId: number): Observable<any> {
+    const bookingData = {
+      flight_id: flightId,
+      status: 'confirmed', // Set the status as needed
+    };
+    return this.http.post(`${this.baseUrl}/bookings`, bookingData);
   }
 }
